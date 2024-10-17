c84566a9fa4b15bf23df61b3e34b720a5a8c165f
José Brito <josealbritose@gmail.com>
2024-10-17 07:17:39 -0300

    Announcements component

---
+++ src/components/shared/admin/Announcements.tsx
@@ -0,0 +1,52 @@
+"use client"
+
+const Announcements = () => {
+  return (
+    <article className="bg-white p-4 rounded-md">
+      <section className="flex items-center justify-between">
+        <h1 className="text-xl font-semibold">Announcements</h1>
+        <button className="text-xs text-gray-800 font-semibold hover:text-gray-500 bg-sky-200 rounded-lg py-1 px-2">View All</button>
+      </section>
+      <section className="flex flex-col gap-4 mt-4">
+        <div className="bg-lamaSkyLight rounded-md p-4">
+          <div className="flex items-center justify-between">
+            <h2 className="font-medium">Lorem ipsum dolor sit</h2>
+            <span className="text-xs text-gray-400 bg-white rounded-md px-1 py-1">
+              2025-01-01
+            </span>
+          </div>
+          <p className="text-sm text-gray-400 mt-1">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum,
+            expedita. Rerum, quidem facilis?
+          </p>
+        </div>
+        <div className="bg-lamaPurpleLight rounded-md p-4">
+          <div className="flex items-center justify-between">
+            <h2 className="font-medium">Lorem ipsum dolor sit</h2>
+            <span className="text-xs text-gray-400 bg-white rounded-md px-1 py-1">
+              2025-01-01
+            </span>
+          </div>
+          <p className="text-sm text-gray-400 mt-1">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum,
+            expedita. Rerum, quidem facilis?
+          </p>
+        </div>
+        <div className="bg-lamaYellowLight rounded-md p-4">
+          <div className="flex items-center justify-between">
+            <h2 className="font-medium">Lorem ipsum dolor sit</h2>
+            <span className="text-xs text-gray-400 bg-white rounded-md px-1 py-1">
+              2025-01-01
+            </span>
+          </div>
+          <p className="text-sm text-gray-400 mt-1">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum,
+            expedita. Rerum, quidem facilis?
+          </p>
+        </div>
+      </section>
+    </article>
+  );
+};
+
+export default Announcements;
