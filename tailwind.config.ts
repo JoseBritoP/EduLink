2b93c97a384e44b2ad44a9919c75e1b681a8fac7
José Brito <josealbritose@gmail.com>
2024-10-17 02:23:03 -0300

    Tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,27 @@
-import type { Config } from "tailwindcss";
+
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  darkMode: 'class',
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      screens:{
+        sm: '640px',
+        md:'768px',
+        lg:'960px',
+        xl:'1200px',
+        xlg: '1600px'
       },
+      colors:{
+        
+      }
     },
   },
   plugins: [],
-};
-export default config;
+}
+export default config
