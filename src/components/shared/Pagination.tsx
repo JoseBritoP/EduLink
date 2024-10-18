d77f9fd61afaeccd6c340ceba3fcb3587a25e8b9
José Brito <josealbritose@gmail.com>
2024-10-18 07:09:43 -0300

    Pagination disabled style

--- src/components/shared/Pagination.tsx
+++ src/components/shared/Pagination.tsx
@@ -14,7 +14,7 @@ export default function Pagination({currentPage,handleNextPage,handlePreviousPag
       <button
         onClick={handlePreviousPage}
         disabled={currentPage === 1}
-        className="text-xs text-gray-800 font-semibold hover:text-gray-50 bg-sky-200 hover:bg-sky-500 rounded-lg py-1 px-2 transition-colors duration-200 ease-linear"
+        className="text-xs text-gray-800 font-semibold hover:text-gray-50 bg-sky-200 hover:bg-sky-500 rounded-lg py-1 px-2 transition-colors duration-200 ease-linear disabled:bg-gray-200 disabled:text-gray-900"
       >
         Previous
       </button>
