dcbf3a68be958222591811bec39432bfcfac7c60
José Brito <josealbritose@gmail.com>
2024-10-21 21:48:47 -0300

    TeacherPage

--- src/app/(dashboard)/teacher/page.tsx
+++ src/app/(dashboard)/teacher/page.tsx
@@ -1,9 +1,26 @@
+import { AnnouncementsStudent, BigCalendar } from '@/components/shared/student'
+import { announcementsData, calendarEvents } from '@/lib/data'
 import React from 'react'
 
 export default function TeacherPage() {
+
+  const teacher = {
+    calendar:calendarEvents,
+    announcements:announcementsData
+  }
   return (
-    <div>
-      Teacher Page
+    <div className="flex-1 p-4 flex gap-4 flex-col xl:flex-row">
+    {/* LEFT */}
+    <div className="w-full xl:w-2/3">
+      <div className="h-full bg-white p-4 rounded-md">
+        <h1 className="text-xl font-semibold">Schedule</h1>
+        <BigCalendar calendarEvents={teacher.calendar} />
+      </div>
     </div>
+    {/* RIGHT */}
+    <div className="w-full xl:w-1/3 flex flex-col gap-8">
+      <AnnouncementsStudent announcements={teacher.announcements}/>
+    </div>
+  </div>
   )
 }
