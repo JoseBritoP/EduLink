efcfe534f9dfbbd5056a186c6af3ee080872db53
José Brito <josealbritose@gmail.com>
2024-10-22 16:53:36 -0300

    FormSchema

---
+++ src/lib/schema/index.ts
@@ -0,0 +1,35 @@
+import z from 'zod'
+
+const username = z
+.string()
+.min(3, { message: "Username must be at least 3 characters long!" })
+.max(20, { message: "Username must be at most 20 characters long!" });
+const email = z.string().email({ message: "Invalid email address!" });
+
+const password = z
+.string()
+.min(8, { message: "Password must be at least 8 characters long!" })
+const firstName = z.string().min(1, { message: "First name is required!" })
+const lastName = z.string().min(1, { message: "Last name is required!" })
+const phone = z.string().min(1, { message: "Phone is required!" })
+const address = z.string().min(1, { message: "Address is required!" })
+const bloodType = z.string().min(1, { message: "Blood Type is required!" })
+const birthday = z.date({ message: "Birthday is required!" })
+const sex = z.enum(["male", "female"], { message: "Sex is required!" })
+const img = z.instanceof(File, { message: "Image is required" })
+
+export const schema = z.object({
+  username,
+  email,
+  password,
+  firstName,
+  lastName,
+  phone,
+  address,
+  bloodType,
+  birthday,
+  sex,
+  img
+});
+
+export type FormSchema = z.infer<typeof schema>
