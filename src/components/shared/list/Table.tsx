746c34cac0bf2652eb030616881eef1e2e3c9562
José Brito <josealbritose@gmail.com>
2024-10-23 07:18:52 -0300

    TableComponent

--- src/components/shared/list/Table.tsx
+++ src/components/shared/list/Table.tsx
@@ -1,12 +1,13 @@
 import { ColumsType, StudentListType, TeacherType } from "@/typescript/types";
 import React from "react";
+import RenderRow from "./RenderRow";
 
 interface TableProps {
   columns: ColumsType;
-  renderRow:  (item: TeacherType | StudentListType) => JSX.Element;
   data: TeacherType[] | StudentListType[]
+  type: 'student' | 'teacher'
 }
-export default function Table({ columns, data, renderRow }: TableProps) {
+export default function Table({ columns, data, type }: TableProps) {
   return (
     <table className="w-full mt-4">
       <thead>
@@ -18,7 +19,7 @@ export default function Table({ columns, data, renderRow }: TableProps) {
           ))}
         </tr>
       </thead>
-      <tbody>{data.map((item) => renderRow(item))}</tbody>
+      <tbody>{data.map((item) => RenderRow({item,type}))}</tbody>
     </table>
   );
 }
