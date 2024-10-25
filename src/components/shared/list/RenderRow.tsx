d8b49fc2aaade38d70aa7db16e36bbcf570a917c
José Brito <josealbritose@gmail.com>
2024-10-25 19:46:44 -0300

    RenderRow

--- src/components/shared/list/RenderRow.tsx
+++ src/components/shared/list/RenderRow.tsx
@@ -1,9 +1,10 @@
-import { Parent, StudentListType, TeacherType } from "@/typescript/types";
+import { Announcement, Assignment, Class, Exam, Lesson, Parent, Result, StudentListType, Subject, TeacherType } from "@/typescript/types";
 import RenderRowTeacherAndStudent from "./RenderRowTeacherAndStudent";
 import RenderRowParent from "./RenderRowParent";
+import RenderRowExtension from "./RenderRowExtension";
 
-interface RenderRowProps {
-  item: TeacherType | StudentListType | Parent;
+export interface RenderRowProps {
+  item: TeacherType | StudentListType | Parent | Announcement | Assignment | Class | Exam | Lesson | Result | Subject;
   type: "teacher" | "student" | "parent" | "subject" | "class" | "lesson" | "exam" | "assignment" | "result" | "attendance" | "event" | "announcement";
 }
 
@@ -14,6 +15,7 @@ const RenderRow = ({ item, type }: RenderRowProps) => (
   >
     {type === 'teacher' ? (<RenderRowTeacherAndStudent item={item as TeacherType} type="teacher"/>) : type === 'student' && <RenderRowTeacherAndStudent item={item as StudentListType} type={type}/>}
     {type === 'parent' && <RenderRowParent item={item as Parent} type="parent"/>}
+    {type !== undefined && <RenderRowExtension item={item}/>}
   </tr>
 );
 
