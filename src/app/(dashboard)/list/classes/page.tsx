22acd54bba82b765081eca18c375e62255a14375
José Brito <josealbritose@gmail.com>
2024-10-25 19:45:44 -0300

    ClassListPage Component

---
+++ src/app/(dashboard)/list/classes/page.tsx
@@ -0,0 +1,60 @@
+import FormModal from "@/components/shared/FormModal";
+import ListTableData from "@/components/shared/list/ListTable";
+import TableSearch from "@/components/shared/list/TableSearch";
+import { classesData, role } from "@/lib/data";
+import Image from "next/image";
+
+
+const columns = [
+  {
+    header: "Class Name",
+    accessor: "name",
+  },
+  {
+    header: "Capacity",
+    accessor: "capacity",
+    className: "hidden md:table-cell",
+  },
+  {
+    header: "Grade",
+    accessor: "grade",
+    className: "hidden md:table-cell",
+  },
+  {
+    header: "Supervisor",
+    accessor: "supervisor",
+    className: "hidden md:table-cell",
+  },
+  {
+    header: "Actions",
+    accessor: "action",
+  },
+];
+
+const ClassListPage = () => {
+
+  return (
+    <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
+      {/* TOP */}
+      <div className="flex items-center justify-between">
+        <h1 className="hidden md:block text-lg font-semibold">All Classes</h1>
+        <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
+          <TableSearch />
+          <div className="flex items-center gap-4 self-end">
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
+              <Image src="/filter.png" alt="" width={14} height={14} />
+            </button>
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
+              <Image src="/sort.png" alt="" width={14} height={14} />
+            </button>
+            {role === "admin" && <FormModal table="class" type="create" />}
+          </div>
+        </div>
+      </div>
+      {/* LIST */}
+      <ListTableData type="class" columns={columns} data={classesData}/>
+    </div>
+  );
+};
+
+export default ClassListPage;
