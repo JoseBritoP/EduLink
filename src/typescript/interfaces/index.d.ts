f0a415101894ef7c7e6f593cafc3192b1a2eef95
José Brito <josealbritose@gmail.com>
2024-10-25 19:45:01 -0300

    Id and table prop en FormSKProps

--- src/typescript/interfaces/index.d.ts
+++ src/typescript/interfaces/index.d.ts
@@ -9,6 +9,20 @@ export interface FormSTProps {
 export interface FormSKProps {
   type: "create" | "update" | "delete";
   data?: Subject | Result | Lesson | Exam | Event | Class | Assignment | Announcement;
+  id?:number
+  table:
+    | "teacher"
+    | "student"
+    | "parent"
+    | "subject"
+    | "class"
+    | "lesson"
+    | "exam"
+    | "assignment"
+    | "result"
+    | "attendance"
+    | "event"
+    | "announcement";
 }
 
 // ----------
@@ -22,4 +36,34 @@ export interface TableProps {
   columns: ColumsType;
   type: "teacher" | "student" | "parent" | "subject" | "class" | "lesson" | "exam" | "assignment" | "result" | "attendance" | "event" | "announcement";
   data: TeacherType[] | StudentListType[] | Parent[] | Announcement[]
+}
+
+
+// 
+
+export type ItemT =
+  | Announcement
+  | Class
+  | Event
+  | Exam
+  | Lesson
+  | Result
+  | Subject
+  | Assignment;
+export type TypeT =
+  | "subject"
+  | "class"
+  | "lesson"
+  | "exam"
+  | "assignment"
+  | "result"
+  | "event"
+  | "announcement";
+export interface RenderRowTeacherAndStudent {
+  item: ItemT;
+  type: TypeT;
+}
+
+export interface RenderRowExtensionProps {
+  item:ItemT
 }
