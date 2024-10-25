d7bf83729b0e21ecaa7711da9e33dc783677e5e7
José Brito <josealbritose@gmail.com>
2024-10-25 19:46:16 -0300

    ResultListPage Component

---
+++ src/app/(dashboard)/list/results/page.tsx
@@ -0,0 +1,73 @@
+
+import FormModal from "@/components/shared/FormModal";
+import ListTableData from "@/components/shared/list/ListTable";
+import TableSearch from "@/components/shared/list/TableSearch";
+import {
+  resultsData,
+  role,
+} from "@/lib/data";
+import Image from "next/image";
+
+
+const columns = [
+  {
+    header: "Subject Name",
+    accessor: "name",
+  },
+  {
+    header: "Student",
+    accessor: "student",
+  },
+  {
+    header: "Score",
+    accessor: "score",
+    className: "hidden md:table-cell",
+  },
+  {
+    header: "Teacher",
+    accessor: "teacher",
+    className: "hidden md:table-cell",
+  },
+  {
+    header: "Class",
+    accessor: "class",
+    className: "hidden md:table-cell",
+  },
+  {
+    header: "Date",
+    accessor: "date",
+    className: "hidden md:table-cell",
+  },
+  {
+    header: "Actions",
+    accessor: "action",
+  },
+];
+
+const ResultListPage = () => {
+
+  return (
+    <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
+      {/* TOP */}
+      <div className="flex items-center justify-between">
+        <h1 className="hidden md:block text-lg font-semibold">All Results</h1>
+        <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
+          <TableSearch />
+          <div className="flex items-center gap-4 self-end">
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
+              <Image src="/filter.png" alt="" width={14} height={14} />
+            </button>
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
+              <Image src="/sort.png" alt="" width={14} height={14} />
+            </button>
+            {role === "admin" || role === "teacher" && <FormModal table="result" type="create" />}
+          </div>
+        </div>
+      </div>
+      {/* LIST */}
+      <ListTableData columns={columns} data={resultsData} type="result"/>
+    </div>
+  );
+};
+
+export default ResultListPage;
