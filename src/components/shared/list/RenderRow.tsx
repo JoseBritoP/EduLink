45b3ecf26e0d558ac9db26534726a61bcaf15844
José Brito <josealbritose@gmail.com>
2024-10-24 21:52:10 -0300

    RenderRowComponent

--- src/components/shared/list/RenderRow.tsx
+++ src/components/shared/list/RenderRow.tsx
@@ -1,71 +1,19 @@
-import Image from "next/image";
-import FormModal from "../FormModal";
-import { role } from "@/lib/data";
-import Link from "next/link";
-import { StudentListType, TeacherType } from "@/typescript/types";
+import { Parent, StudentListType, TeacherType } from "@/typescript/types";
+import RenderRowTeacherAndStudent from "./RenderRowTeacherAndStudent";
+import RenderRowParent from "./RenderRowParent";
 
 interface RenderRowProps {
-  item: TeacherType | StudentListType;
-  type: "teacher" | "student";
+  item: TeacherType | StudentListType | Parent;
+  type: "teacher" | "student" | "parent" | "subject" | "class" | "lesson" | "exam" | "assignment" | "result" | "attendance" | "event" | "announcement";
 }
-
-const isTeacher = (item: TeacherType | StudentListType): item is TeacherType => {
-  return (item as TeacherType).teacherId !== undefined;
-};
 
 const RenderRow = ({ item, type }: RenderRowProps) => (
   <tr
     key={item.id}
     className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPurpleLight"
   >
-    <td className="flex items-center gap-4 p-1 md:p-4">
-      <Image
-        src={item.photo}
-        alt={`${item.name}_img`}
-        width={40}
-        height={40}
-        className="md:hidden xl:block w-10 h-10 rounded-full object-cover"
-      />
-      <div className="flex flex-col">
-        <h3 className="font-semibold">{item.name}</h3>
-        <p className="text-xs text-gray-500">{item?.email}</p>
-      </div>
-    </td>
-    
-    {isTeacher(item) ? (
-      <>
-        <td className="hidden md:table-cell">{item.teacherId}</td>
-        <td className="hidden md:table-cell">{item.subjects.join(" - ")}</td>
-        <td className="hidden md:table-cell">{item.classes.join(" - ")}</td>
-        <td className="hidden lg:table-cell">{item.phone}</td>
-        <td className="hidden xl:table-cell">{item.address}</td>
-      </>
-    ) : (
-      <>
-        <td className="hidden md:table-cell">{item.studentId}</td>
-        <td className="hidden md:table-cell">{item.grade}</td>
-        <td className="hidden md:table-cell">{item.phone}</td>
-        <td className="hidden md:table-cell">{item.address}</td>
-      </>
-    )}
-
-    <td>
-      <div className="flex items-center gap-2">
-        <Link href={`/list/${type}s/${item.id}`}>
-          <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaSky">
-            <Image src="/view.png" alt="view_logo" width={16} height={16} />
-          </button>
-        </Link>
-        {role === "admin" && (
-          <>
-            <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaPurple">
-              <Image src="/delete.png" alt="" width={16} height={16} />
-            </button>
-            <FormModal table={type} type="delete" id={item.id} />
-          </>
-        )}
-      </div>
-    </td>
+    {type === 'teacher' ? (<RenderRowTeacherAndStudent item={item as TeacherType} type="teacher"/>) : type === 'student' && <RenderRowTeacherAndStudent item={item as StudentListType} type={type}/>}
+    {type === 'parent' && <RenderRowParent item={item as Parent} type="parent"/>}
   </tr>
 );
 
