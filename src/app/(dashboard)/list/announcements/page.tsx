c5e0593eaf80f4eaed703d9c7c59d4f555f5104a
José Brito <josealbritose@gmail.com>
2024-10-25 19:12:23 -0300

    AnnouncementPage

---
+++ src/app/(dashboard)/list/announcements/page.tsx
@@ -0,0 +1,58 @@
+import FormModal from "@/components/shared/FormModal";
+import ListTableData from "@/components/shared/list/ListTable";
+import TableSearch from "@/components/shared/list/TableSearch";
+import { announcementsData, role } from "@/lib/data";
+import Image from "next/image";
+
+const columns = [
+  {
+    header: "Title",
+    accessor: "title",
+  },
+  {
+    header: "Class",
+    accessor: "class",
+  },
+  {
+    header: "Date",
+    accessor: "date",
+    className: "hidden md:table-cell",
+  },
+  {
+    header: "Actions",
+    accessor: "action",
+  },
+];
+
+const AnnouncementListPage = () => {
+  
+
+  return (
+    <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
+      {/* TOP */}
+      <div className="flex items-center justify-between">
+        <h1 className="hidden md:block text-lg font-semibold">
+          All Announcements
+        </h1>
+        <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
+          <TableSearch />
+          <div className="flex items-center gap-4 self-end">
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
+              <Image src="/filter.png" alt="" width={14} height={14} />
+            </button>
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
+              <Image src="/sort.png" alt="" width={14} height={14} />
+            </button>
+            {role === "admin" && (
+              <FormModal table="announcement" type="create" />
+            )}
+          </div>
+        </div>
+      </div>
+      {/* LIST */}
+      <ListTableData columns={columns} data={announcementsData} type="announcement"/>
+    </div>
+  );
+};
+
+export default AnnouncementListPage;
