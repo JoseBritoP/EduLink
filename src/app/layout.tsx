b35a59e307eb27f8ba0121461de6f796b1bb7bf3
José Brito <josealbritose@gmail.com>
2024-10-26 17:31:10 -0300

    Landing Layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import { Roboto } from "next/font/google";
 import { ThemeProvider } from "@/components/ui/ThemeDarkModeProvider";
 import Navbar from "@/components/landing/Navbar";
+import Footer from "@/components/landing/Footer";
 
 const roboto = Roboto({
   weight: "400",
@@ -25,15 +26,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${roboto.className} antialiased h-screen w-full dark:bg-gray-950`}>
+      <body
+        className={`${roboto.className} antialiased h-screen w-full dark:bg-gray-950`}
+      >
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
           enableSystem
           disableTransitionOnChange
         >
-          <Navbar/>
-          {children}
+          <div className="flex flex-col justify-between">
+            <Navbar />
+            {children}
+            <Footer />
+          </div>
         </ThemeProvider>
       </body>
     </html>
