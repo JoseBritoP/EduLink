8fd49bc62d3effa1c1ead12f3da509df35f524c2
José Brito <josealbritose@gmail.com>
2024-10-26 17:31:16 -0300

    HomeLandingPage

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import { CarouselComponent } from "@/components/landing/Carousel";
+import Inscriptions from "@/components/landing/Inscriptions";
+import LearningPlatform from "@/components/landing/LearningPlatform";
+import LevelsOfEducation from "@/components/landing/LevelsOfEducation";
+import Solidary from "@/components/landing/Solidary";
+import React from "react";
 
 export default function HomePage() {
   return (
-    <main>
-      Hello World    
+    <main className="flex flex-col justify-center items-center flex-1 py-5 dark:bg-darkGray gap-y-10">
+      <CarouselComponent />
+      <LevelsOfEducation/>
+      <Inscriptions/>
+      <Solidary/>
+      <LearningPlatform/>
     </main>
-  )
+  );
 }
