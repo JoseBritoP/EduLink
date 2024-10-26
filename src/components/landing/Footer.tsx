521e4da870efb9d73a83b1f27588ef530267447b
José Brito <josealbritose@gmail.com>
2024-10-26 17:31:37 -0300

    Footer

--- src/components/landing/Footer.tsx
+++ src/components/landing/Footer.tsx
@@ -1,11 +1,15 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 export default function Footer() {
   const size = 40;
   return (
-    <footer className="flex bg-veryDarkGray h-[120px] justify-center items-center gap-x-20">
+    <footer className="flex bg-veryDarkGray dark:bg-gray-950 h-[120px] justify-center items-center gap-x-20 text-gray-200">
+      <Link href='/' className="flex flex-col items-center justify-center gap-y-1 ">
       <Image src="/logo.png" alt="logo" height={size} width={size} />
+      <span className="font-semibold text-xl">EduLink</span>
+      </Link>
       <section>
         <p className="text-lg font-medium">School EduLink</p>
         <p className="text-lg font-medium">
